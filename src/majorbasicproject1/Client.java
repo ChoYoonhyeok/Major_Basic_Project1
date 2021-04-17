b576f72c07e270e3bc1d7a7cbc272587c8ec3c98
YJLII <destprato@gmail.com>
2021-04-17 23:09:17 +0900

    Client 필요한 기능 있을지 윤곽 추가

--- src/majorbasicproject1/Client.java
+++ src/majorbasicproject1/Client.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 
 public class Client {
 	static Scanner scan = new Scanner(System.in);
-	static ArrayList<ProductManager> shopping_basket;		//장바구니 ArrayList
+	static ArrayList<Product, Integer> shopping_basket;		//장바구니 ArrayList
 	static ProductManager productManager;						//Products class
 	
 	Client() {
@@ -16,10 +16,11 @@ public class Client {
 	
 	void printClientMenu() {
 		productManager.printProductsMenu();
-		System.out.print("구매할 상품을 선택하세요: ");
-		addBasket(scan.next());
+		System.out.print("구매할 상품과 수량. 예) Whopper,3");
+		addBasket(scan.next());//이 줄에 대한 입력은 위 함수를 , 통해 쪼갠 후 parseInt 작업, 제네릭에 전달
 		
 		// TODO: 사용자가 주문한 상품의 가격 및 수량과 합계가격, [1. 상품주문수정/2. 결제]메뉴 출력
+		//************장바구니 기능 구현 어디에 해야 할 지?
 		System.out.println("1. 상품주문수정\n2. 결제");
 		selectMenu(scan.next());
 	}
@@ -43,6 +44,7 @@ public class Client {
 	
 	void changeBasket() {
 		//TODO: 상품주문수정
+		
 		System.out.println("상품이 변경되었습니다.");
 	}
 	
@@ -51,5 +53,7 @@ public class Client {
 		System.out.println("결제되었습니다.");
 	}
 	
-
+	void showBasket(){
+		//장바구니 볼 수 있는 기능 추가.
+	}
 }
